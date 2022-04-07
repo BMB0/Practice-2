e8720a0e7449d6934489a7e8b2fa46db31466f7d
BMB <bruno.marquez@ucb.edu.bo>
2022-04-07 17:14:54 -0400

    P2-001 Added Logic

---
+++ Logic/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Logic/UserManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class UserManager
+    {
+        public List<User> Users { get; set; }
+
+        public UserManager()
+        {
+            Users = new List<User>
+            {
+                new User() { Name = "Bruno" },
+                new User() { Name = "Andres" }
+            };
+        }
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
+        public User PostUsers(User user)
+        {
+            Users.Add(user);
+            return user;
+        }
+        public User PutUsers(User user)
+        {
+            return null;
+        }
+        public User DeleteUsers(User user)
+        {
+            return null;
+        }
+    }
+}
